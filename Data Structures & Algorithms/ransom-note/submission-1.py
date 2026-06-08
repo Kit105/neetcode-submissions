fb16bf10cf06a42266de5d86a289b231c7701f74
Ankit Basare <40236497+Kit105@users.noreply.github.com>
2026-06-08 12:59:46 +0530

    Add: ransom-note - submission-1

---
+++ Data Structures & Algorithms/ransom-note/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(ransomNote) > len(magazine):
+            return False
+        
+        res = [0] * 26
+
+        for m in magazine:
+            res[ord(m) - ord('a')] += 1
+
+        for r in ransomNote:
+            res[ord(r) - ord('a')] -= 1
+            if res[ord(r) - ord('a')] < 0:
+                return False
+
+        return True
